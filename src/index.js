b66cad999dcabc4d89a64d9d9f5610ece8e81426
Tanya Selvog <tanyaselvog@gmail.com>
2021-04-04 14:27:25 -0500

    added handleClick to Board class

--- src/index.js
+++ src/index.js
@@ -30,6 +30,12 @@ class Square extends React.Component {
           };
         }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
 
     renderSquare(i) {
       return (
